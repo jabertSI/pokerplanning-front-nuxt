c6ac71df206fd736fdb469398de1ebe7f001a7dd
jabo <j.sichaib@live.fr>
2025-12-05 11:06:45 +0100

    feat(username): add username component with persisted store

---
+++ app/stores/userStore.ts
@@ -0,0 +1,15 @@
+export const useUserStore = defineStore(
+  "user",
+  () => {
+    const username = ref<string | null>(null);
+    const vote = ref<number | null>(null);
+    const roomUuid = ref(null);
+
+    return { username, vote };
+  },
+  {
+    persist:{
+      omit: ['vote' ,'roomUuid'],
+    },
+  },
+);
